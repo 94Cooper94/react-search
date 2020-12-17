2e2d3b4322c1554b90a9e34afa1b6934350f62d0
Cooper <eliteswimmer42@gmail.com>
2020-12-17 15:37:28 -0600

    updating readme and dependencies in package.json

---
+++ client/build/precache-manifest.410aad2f0c7aeb2976e295adb07ba39f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0548dd7a5f84eb8b71f1bfafe87e215",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a3ca1f18e9e8d854d0a",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "370988744a9548414196",
+    "url": "/static/js/2.e6c877b0.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.e6c877b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a3ca1f18e9e8d854d0a",
+    "url": "/static/js/main.48bc87bc.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
